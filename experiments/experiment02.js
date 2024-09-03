3b6424b8d2f0cc19eb8f935c1d20bb42e2abd3ef
DavWes <weda18pc@student.ju.se>
2024-09-03 04:49:19 +0200

    commit

--- experiments/experiment02.js
+++ experiments/experiment02.js
@@ -2,21 +2,30 @@ function setup() {
   createCanvas(1000, 1000);
 }
 
-const size = 40;
+const size = 5;
 const divider = 10;
-const numRows = 10;
-const numCols = 10;
+const numRows = 100;
+const numCols = 100;
 
 function draw() {
   background(255, 255, 255);
   noStroke();
   fill(0, 0, 0);
 
-  // noiseSeed(0);
   for (let y = 0; y < numRows; y++) {
     for (let x = 0; x < numCols; x++) {
       const value = noise(x / divider, y / divider) * size;
-      ellipse(size / 2 + x * size, size / 2 + y * size, value);
+      const posX = size / 2 + x * size;
+      const posY = size / 2 + y * size;
+
+      if (random() < 0.5) {
+        // Draw an ellipse
+        ellipse(posX, posY, value);
+      } else {
+        // Draw a square
+        rectMode(CENTER);
+        rect(posX, posY, value, value);
+      }
     }
   }
 
